b6e9dc96965ed0ccde9f30cc1b5862b4088499ad
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2020-02-18 11:31:02 +0000

    Initial analysis commit

---
+++ llvm/lib/Analysis/OverflowTracking.cpp
@@ -0,0 +1,40 @@
+//===- OverflowTrackig.cpp - Testing integer overflow analysis ---------------===//
+//
+// No license at the moment.
+// Justs Zarins
+// j.zarins@epcc.ed.ac.uk
+//
+//===----------------------------------------------------------------------===//
+//
+// This class is for testing and learning on the way to integer overflow analysis.
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+using namespace llvm;
+
+#define DEBUG_TYPE "hello"
+
+STATISTIC(HelloCounter, "Counts number of functions greeted");
+
+namespace {
+  // Hello - The first implementation, without getAnalysisUsage.
+  struct Hello : public FunctionPass {
+    static char ID; // Pass identification, replacement for typeid
+    Hello() : FunctionPass(ID) {}
+
+    bool runOnFunction(Function &F) override {
+      ++HelloCounter;
+      errs() << "Hello: ";
+      errs().write_escaped(F.getName()) << '\n';
+      return false;
+    }
+  };
+}
+
+char Hello::ID = 0;
+static RegisterPass<Hello> X("test", "Hello World Pass test");
+
