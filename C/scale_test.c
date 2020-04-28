b9e7af6c9fb825537a28ebcf2d41159a43f415e0
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2020-04-28 12:18:31 +0100

    Expand C MWE

--- C/scale_test.c
+++ C/scale_test.c
@@ -8,9 +8,13 @@ int __attribute__ ((noinline)) calc_mostly_ok(int r) {
     return r * BIG_NUM;
 }
 
+int __attribute__ ((noinline)) calc_mostly_ok_pointer(int* r) {
+    return *r * BIG_NUM;
+}
+
 int main(int argc, char *argv[]) {
     MPI_Init(NULL, NULL);
-    int i, rank, size, indirect_overflow, direct_overflow, mostly_ok_int, intermediate_calc, intermediate_end, pointer_calc;
+    int i, rank, size, indirect_overflow, direct_overflow, mostly_ok_int, intermediate_calc, intermediate_end, pointer_calc, func_pointer_calc;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
@@ -33,9 +37,10 @@ int main(int argc, char *argv[]) {
 
     int* rankp = &(rank);
     pointer_calc = *rankp * BIG_NUM;
+    func_pointer_calc = calc_mostly_ok_pointer(rankp);
 
-    printf("Rank %d; mostly_ok_int=%d, direct_overflow=%d, indirect_overflow=%d, user_input_result=%d, intermediate_end=%d, pointer_calc=%d\n", 
-           rank, mostly_ok_int, direct_overflow, indirect_overflow, user_input_result, intermediate_end, pointer_calc);
+    printf("Rank %d; mostly_ok_int=%d, direct_overflow=%d, indirect_overflow=%d, user_input_result=%d, intermediate_end=%d, pointer_calc=%d, func_pointer_calc=%d\n", 
+           rank, mostly_ok_int, direct_overflow, indirect_overflow, user_input_result, intermediate_end, pointer_calc, func_pointer_calc);
 
     MPI_Finalize();
     return 0;
