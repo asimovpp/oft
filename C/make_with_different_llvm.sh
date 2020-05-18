65a47ba98c4eb2acb85c142e6bad055eda46eccb
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2020-05-18 19:38:46 +0100

    Move info printing into Makefile

--- C/make_with_different_llvm.sh
+++ C/make_with_different_llvm.sh
@@ -4,10 +4,4 @@ llvm=/home/jzarins/sources/llvm-project/build-v10-release/
 #llvm=/home/jzarins/sources/llvm-project/build-master/
 export PATH=$llvm/bin:$PATH
 
-echo $PATH
-flang --version
-clang --version
-llvm-dis --version
-#ompi_info
-
 make
