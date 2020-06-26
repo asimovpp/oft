3bcbc0494616554acd0d6ee2fc8b39c9ca9c1c38
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2020-06-26 11:22:33 +0100

    Change instrumentation to use MPI

--- C/llvm_analysis_funcs.c
+++ C/llvm_analysis_funcs.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
 //LLVM would be able to change the size of this table as needed
 #define TABLE_LEN 100
 
 int values[TABLE_LEN];
+FILE *fptr;
 
 void print_val(int v) {
     printf("Value is %d\n", v);
@@ -16,14 +18,27 @@ void init_vals() {
 
 void store_max_val(int id, int v) {
     if (v > values[id]) {
-        printf("Instrumentation: Setting val %d to %d\n", id, v);
+        //printf("Instrumentation: Setting val %d to %d\n", id, v);
         values[id] = v;
     }
 }
 
 void print_max_vals() {
     int rank = -1;
-    //MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    for (int i = 0; i < TABLE_LEN; i++) 
-        printf("Rank %d; Value %d maxed at %d\n", rank, i, values[i]);
+    int world_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int* all_rank_values = malloc(world_size * TABLE_LEN * sizeof(int));
+    MPI_Gather(values, TABLE_LEN, MPI_INT, all_rank_values, TABLE_LEN, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        fptr = fopen("overflow_tool_output.txt", "w");
+        for (int i = 0; i < world_size; i++) {
+            for (int j = 0; j < TABLE_LEN; j++) {
+                fprintf(fptr, "Rank %d; Value %d maxed at %d\n", i, j, all_rank_values[i * TABLE_LEN + j]);
+            }
+        }
+        fclose(fptr);
+    }
 }
