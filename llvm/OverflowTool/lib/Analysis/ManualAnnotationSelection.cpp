7ab032cfec02ddf13685406b3b2b3aeff053eff7
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2020-12-01 17:15:13 +0000

    Fix compilation error with non-debug LLVM builds

--- llvm/OverflowTool/lib/Analysis/ManualAnnotationSelection.cpp
+++ llvm/OverflowTool/lib/Analysis/ManualAnnotationSelection.cpp
@@ -24,7 +24,7 @@ ManualAnnotationSelection::perform(const llvm::Module &CurModule) {
   ManualAnnotationSelection::Result res{nullptr};
   // if (F.isDeclaration()) {
   LLVM_DEBUG(llvm::dbgs() << "processing module: " << CurModule.getName()
-                          << '\n';);
+                          << "\n";);
   // return false;
   if (CurModule.getFunctionList().size()) {
     firstFunc = &*CurModule.functions().begin();
@@ -35,7 +35,7 @@ ManualAnnotationSelection::perform(const llvm::Module &CurModule) {
   }
 
   LLVM_DEBUG(llvm::dbgs() << "processing func: " << firstFunc->getName()
-                          << '\n';);
+                          << "\n";);
   // LLVM_DEBUG(llvm::dbgs() << "processing func: " << F.getName() << '\n';);
   res.theInstruction = &*firstFunc->begin()->begin();
 
