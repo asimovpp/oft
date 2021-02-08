9660865401a4a2e4ee51c4abf628e4f68ec44641
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2021-02-08 18:09:19 +0000

    Clean up example unit test

--- llvm/OverflowTool/tests/unittests/test.cpp
+++ llvm/OverflowTool/tests/unittests/test.cpp
@@ -14,11 +14,10 @@ unsigned int Factorial(unsigned int number) {
   return number <= 1 ? number : Factorial(number - 1) * number;
 }
 
-TEST_CASE("Factorials are computed", "[factorial]") {
+TEST_CASE("Factorials are computed", "[factorial],[example]") {
   REQUIRE(Factorial(1) == 1);
   REQUIRE(Factorial(2) == 2);
   REQUIRE(Factorial(3) == 6);
-  REQUIRE(Factorial(10) == 3628800);
 }
 
 TEST_CASE("Manual annotation", "[llvmir],[manual],[annotation]") {
