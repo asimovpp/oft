81c4c669c11b0b4fa7af48f82e3875723ac9541d
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2021-02-08 18:09:19 +0000

    Add section and ability to add instructions to module under test

--- llvm/OverflowTool/tests/unittests/test.cpp
+++ llvm/OverflowTool/tests/unittests/test.cpp
@@ -10,6 +10,8 @@
 #include "llvm/IR/Module.h"
 #include "llvm/Support/SourceMgr.h"
 
+#include <string>
+
 unsigned int Factorial(unsigned int number) {
   return number <= 1 ? number : Factorial(number - 1) * number;
 }
@@ -21,31 +23,43 @@ TEST_CASE("Factorials are computed", "[factorial],[example]") {
 }
 
 TEST_CASE("Manual annotation", "[llvmir],[manual],[annotation]") {
-  llvm::LLVMContext C;
+  llvm::LLVMContext ctx;
 
-  const char *ModuleString = R"(
+  const std::string moduleStringStart = R"(
 @g = common dso_local global i32 0
 
 define void @f(i32 %x) {
   entry:
     %v0 = alloca i32, align 4
     %v1 = add i32 1, 1
-    %v2 = add i32 %x, 2
+    %v2 = add i32 %x, 2)";
+
+  const std::string moduleStringEnd = R"(
     ret void
 })";
 
-  llvm::SMDiagnostic Err;
-  std::unique_ptr<llvm::Module> M =
-      llvm::parseAssemblyString(ModuleString, Err, C);
-  llvm::Function *F = M->getFunction("f");
+  llvm::SMDiagnostic err;
 
-  REQUIRE(F != nullptr);
-  REQUIRE(F->arg_begin() != F->arg_end());
+  SECTION("annotating stack variable") {
+    const std::string moduleStringAnnotations = R"(
+    %castv0 = bitcast i32* %v0 to i8*
+)";
 
-  llvm::BasicBlock &entryBB = F->front();
-  auto it = entryBB.begin();
-  llvm::Instruction &I0 = *(it);
-  llvm::Instruction &I1 = *(++it);
+    auto moduleString =
+        moduleStringStart + moduleStringAnnotations + moduleStringEnd;
 
-  llvm::Argument &X = *F->arg_begin();
+    std::unique_ptr<llvm::Module> curMod =
+        llvm::parseAssemblyString(moduleString, err, ctx);
+    llvm::Function *func = curMod->getFunction("f");
+
+    REQUIRE(func != nullptr);
+    REQUIRE(func->arg_begin() != func->arg_end());
+
+    llvm::BasicBlock &entryBB = func->front();
+    auto it = entryBB.begin();
+    llvm::Instruction &i0 = *(it);
+    llvm::Instruction &i1 = *(++it);
+
+    llvm::Argument &x = *func->arg_begin();
+  }
 }
