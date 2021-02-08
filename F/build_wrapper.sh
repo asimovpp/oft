da5709b466623665a15630cd2e5d3cf80f28ead9
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-02-08 18:45:44 +0000

    Clean up Fortran examples Makefile and build wrapper

--- F/build_wrapper.sh
+++ F/build_wrapper.sh
@@ -1,11 +1,8 @@
-llvm=/home/jzarins/install/llvm-old-flang/
-export PATH=$llvm/bin:$PATH
-
-make clean
-make use_module.exe
+#make obj
 make ll
 
 cp /home/jzarins/sources/llvm_hpc_static_analysis/C/llvm_analysis_funcs.ll .
 llvm-link -S -o use_module_linked.ll use_module.ll set_module.ll scale_module.ll llvm_analysis_funcs.ll
+llvm-link -S -o annotate_test_linked.ll annotate_test.ll llvm_analysis_funcs.ll
 
-#make all
+make exe
