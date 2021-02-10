e648b1f4cb4b981982116db9ca59b3488fecd532
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2021-02-10 19:04:28 +0000

    Factor out main to speed up compilation

--- llvm/OverflowTool/tests/unittests/test.cpp
+++ llvm/OverflowTool/tests/unittests/test.cpp
@@ -1,6 +1,3 @@
-// This tells Catch to provide a main() - do this once in a single source file
-#define CATCH_CONFIG_MAIN
-
 #include "catch2/catch.hpp"
 
 #include "OverflowTool/Analysis/ManualAnnotationSelection.hpp"
