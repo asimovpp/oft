e9dd63d7a8319de81df44133a6114d0572ec0565
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2021-02-16 21:49:53 +0000

    Add regression test support

---
+++ llvm/OverflowTool/tests/regression/annotation_test.c
@@ -0,0 +1,13 @@
+// RUN: clang -c -O1 -S -emit-llvm %s -o %t1.ll
+// RUN: grep "@oft_mark(" %t1.ll | grep call
+#include <stdio.h>
+
+extern void oft_mark(void *);
+
+int main() {
+  int a_number = 42;
+  oft_mark(&a_number);
+
+  printf("Local num is %d\n", a_number * 7);
+  return 0;
+}
