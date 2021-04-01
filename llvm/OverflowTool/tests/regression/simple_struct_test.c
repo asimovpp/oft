2c54b5943850fd31f39515115f940a89ab2d55c9
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-04-01 12:37:31 +0100

    Add simple struct test and remove MPI from tests where unnecessary

--- llvm/OverflowTool/tests/regression/simple_struct_test.c
+++ llvm/OverflowTool/tests/regression/simple_struct_test.c
@@ -1,9 +1,7 @@
 // RUN: clang -g -c -O0 -Xclang -disable-O0-optnone -S -emit-llvm %s -o %t1.ll
 // RUN: opt -load-pass-plugin %libdir/libLLVMOverflowToolPass.so -aa-pipeline='basic-aa' -passes='oft-overflow-instrumentation' -S -o %t1.instrumented.ll %t1.ll 2> %t1.passout.ll
-// RUN: grep ".*given to.*Line 26.*" %t1.passout.ll
-// RUN: grep ".*given to.*Line 27.*" %t1.passout.ll
-
-// OLD: x=$( grep "given to" %t1.passout.ll | wc -l ); [ $x -eq 2 ]
+// RUN: grep ".*given to.*Line 21.*" %t1.passout.ll
+// RUN: grep ".*given to.*Line 22.*" %t1.passout.ll
 
 #include <stdio.h>
 extern void oft_mark_(void *);
@@ -13,20 +11,16 @@ struct scale_vars {
     int size;
 };
 
-void __attribute__((noinline)) set_struct_scale_var(struct scale_vars *sv) {
-    oft_mark_(&(sv->size));
-    oft_mark_(&(sv->rank));
-}
-
 int main() {
     int struct_res1, struct_res2;
     struct scale_vars sv;
 
-    set_struct_scale_var(&sv);
+    oft_mark_(&(sv.size));
+    oft_mark_(&(sv.rank));
+    
     struct_res1 = sv.rank * 3; //multiplication should be marked as a potential scale overflow
     struct_res2 = sv.size * 7; //multiplication should be marked as a potential scale overflow
 
     printf("struct_res1=%d, struct_res2=%d\n", struct_res1, struct_res2);
-
     return 0;
 }
