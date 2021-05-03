f4d03c2cc764bd5642c8961937610a7c8a670982
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-05-03 13:44:23 +0100

    Add argv user input test

---
+++ llvm/OverflowTool/tests/regression/argv_input.c
@@ -0,0 +1,13 @@
+// RUN: clang -g -c -O0 -Xclang -disable-O0-optnone -S -emit-llvm %s -o %t1.ll
+// RUN: opt -load-pass-plugin %libdir/libLLVMOverflowToolPass.so -aa-pipeline='basic-aa' -passes='oft-overflow-instrumentation' -S -o %t1.instrumented.ll %t1.ll 2> %t1.passout.ll
+// RUN: %bindir/check_marked_lines %t1.passout.ll 11
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int user_input;
+    if (argc == 2)
+        user_input = atoi(argv[1]);
+    printf("Number is %d\n", user_input * 7); //mul should be marked
+    return 0;
+}
