5dc336ff61e124565e109d7211100cca283eb3e8
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-05-04 09:29:10 +0100

    Replace MPI calls with oft_mark

--- llvm/OverflowTool/tests/regression/allocate_test.f90
+++ llvm/OverflowTool/tests/regression/allocate_test.f90
@@ -1,23 +1,17 @@
-! RUN: mpifort -g -c -O0 -Xclang -disable-O0-optnone -S -emit-llvm %s -o %t1.ll
+! RUN: flang -g -c -O0 -Xclang -disable-O0-optnone -S -emit-llvm %s -o %t1.ll
 ! RUN: opt -load-pass-plugin %libdir/libLLVMOverflowToolPass.so -aa-pipeline='basic-aa' -passes='oft-overflow-instrumentation' -S -o %t1.instrumented.ll %t1.ll 2>%t1.passout.ll
-! RUN: %bindir/check_marked_lines %t1.passout.ll 18 19 
+! RUN: %bindir/check_marked_lines %t1.passout.ll 15 16 
 
 
 program use_module
-    
     implicit none
-    include 'mpif.h'
+    external oft_mark
     integer, dimension (:), allocatable :: array
-    integer ierr
-    call initialise_mpi()
     
     allocate(array(2))
-    call MPI_Comm_rank(MPI_COMM_WORLD, array(1), ierr)    
-    call MPI_Comm_rank(MPI_COMM_WORLD, array(2), ierr)    
+    call oft_mark(array(1))
+    call oft_mark(array(2))
 
     write(*,*) "Result 1: ", 3 * array(1) !mul should be marked
     write(*,*) "Result 2: ", 7 * array(2) !mul should be marked
-
-    call finalise_mpi()
-
 end program use_module
--- llvm/OverflowTool/tests/regression/malloc_test.f90
+++ llvm/OverflowTool/tests/regression/malloc_test.f90
@@ -1,26 +1,19 @@
-! RUN: mpifort -g -c -O0 -Xclang -disable-O0-optnone -S -emit-llvm %s -o %t1.ll
+! RUN: flang -g -c -O0 -Xclang -disable-O0-optnone -S -emit-llvm %s -o %t1.ll
 ! RUN: opt -load-pass-plugin %libdir/libLLVMOverflowToolPass.so -aa-pipeline='basic-aa' -passes='oft-overflow-instrumentation' -S -o %t1.instrumented.ll %t1.ll 2>%t1.passout.ll
-! RUN: %bindir/check_marked_lines %t1.passout.ll 21 22 
+! RUN: %bindir/check_marked_lines %t1.passout.ll 17 18 
 
 program malloc_test
-
     implicit none
-    include 'mpif.h'
+    external oft_mark
     integer :: scale_array(*) 
-    integer ierr
     external op_malloc ! the definition should be in an external C file, but for the test here it is not needed
     integer*8 :: op_malloc  
     pointer(ptr_arr, scale_array)
     ptr_arr = op_malloc(2*4)
 
-    call initialise_mpi()
-    
-    call MPI_Comm_rank(MPI_COMM_WORLD, scale_array(1), ierr)    
-    call MPI_Comm_size(MPI_COMM_WORLD, scale_array(2), ierr)
+    call oft_mark(scale_array(1))
+    call oft_mark(scale_array(2))
     
     write(*,*) "Result 1: ", 3 * scale_array(1) !mul should be marked
     write(*,*) "Result 2: ", 7 * scale_array(2) !mul should be marked
-
-    call finalise_mpi()
-
 end program malloc_test
