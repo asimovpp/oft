5340ea078041cd27a8783566344fe69beb1fce45
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-05-04 09:00:35 +0100

    Add Fortran annotation test

---
+++ llvm/OverflowTool/tests/regression/annotation_test.f90
@@ -0,0 +1,11 @@
+! RUN: flang -g -c -O0 -Xclang -disable-O0-optnone -S -emit-llvm %s -o %t1.ll
+! RUN: opt -load-pass-plugin %libdir/libLLVMOverflowToolPass.so -aa-pipeline='basic-aa' -passes='oft-overflow-instrumentation' -S -o %t1.instrumented.ll %t1.ll 2>%t1.passout.ll
+! RUN: %bindir/check_marked_lines %t1.passout.ll 10 
+
+program hello
+    implicit none
+    external oft_mark
+    integer val
+    call oft_mark(val)
+    print*, 'Number is', val * 3 !mul should be marked
+end
