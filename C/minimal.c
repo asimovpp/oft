78a6bfc493f2b28337d31ac51ae435293792b9b0
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-05-12 14:08:21 +0100

    Removed separate C and Fortran examples because they are now regression tests

--- C/minimal.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#define BIG_NUM 268435456
-
-int main() {
-  int to_print = BIG_NUM;
-  printf("Big num is %d\n", to_print);
-  return 0;
-}
--- F/build_wrapper.sh
+++
@@ -1,8 +0,0 @@
-#make obj
-make ll
-
-cp /home/jzarins/sources/llvm_hpc_static_analysis/C/llvm_analysis_funcs.ll .
-llvm-link -S -o use_module_linked.ll use_module.ll set_module.ll scale_module.ll llvm_analysis_funcs.ll
-llvm-link -S -o annotate_test_linked.ll annotate_test.ll llvm_analysis_funcs.ll
-
-make exe
