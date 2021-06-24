cd9d8431f61e73371ee0521871e9be447a572186
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-06-24 14:53:17 +0100

    Use heap instead of stack in test

--- llvm/OverflowTool/tests/regression/indirect_array_access.c
+++ llvm/OverflowTool/tests/regression/indirect_array_access.c
@@ -1,7 +1,8 @@
 // RUN: clang -g -c -O0 -Xclang -disable-O0-optnone -S -emit-llvm %s -o %t1.ll
 // RUN: opt -load-pass-plugin %libdir/libLLVMOverflowToolPass.so -aa-pipeline='basic-aa' -passes='oft-overflow-instrumentation' -S -o %t1.instrumented.ll %t1.ll 2> %t1.passout.ll
-// RUN: %bindir/check_marked_lines %t1.passout.ll 16 17
+// RUN: %bindir/check_marked_lines %t1.passout.ll 17 18
 
+#include <stdlib.h>
 #include <stdio.h>
 extern void oft_mark_(void *);
 
@@ -11,7 +12,7 @@ void __attribute__((noinline)) set_array(int* array) {
 }
 
 int main() {
-    int rank_array[4];
+    int *rank_array = malloc(sizeof(int) * 4);
     set_array(rank_array);
     printf("Number is %d\n", rank_array[0] * 3); //mul should be marked
     printf("Number is %d\n", rank_array[1] * 7); //mul should be marked
