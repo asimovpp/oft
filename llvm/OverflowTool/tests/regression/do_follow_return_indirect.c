35bf255a2ad8c53057e9f25c4fd8ba14ca51270e
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-08-10 12:20:36 +0100

    Add indirect return-follow test

---
+++ llvm/OverflowTool/tests/regression/do_follow_return_indirect.c
@@ -0,0 +1,19 @@
+// RUN: clang -g -c -O0 -Xclang -disable-O0-optnone -S -emit-llvm %s -o %t1.ll
+// RUN: opt -load-pass-plugin %libdir/libLLVMOverflowToolPass.so -aa-pipeline='basic-aa' -passes='oft-overflow-instrumentation' -S -o %t1.instrumented.ll %t1.ll 2> %t1.passout.ll
+// RUN: %bindir/check_marked_lines %t1.passout.ll 10 17 
+
+#include <stdio.h>
+extern void oft_mark_(void *);
+int x;
+
+int __attribute__ ((noinline)) do_calc() {
+    return x * 7; //mul should be marked
+}
+
+int main() {
+    int value;
+    oft_mark_(&x);
+    value = do_calc();
+    printf("Number is %d\n", value * 3); //mul should be marked
+    return 0;
+}
