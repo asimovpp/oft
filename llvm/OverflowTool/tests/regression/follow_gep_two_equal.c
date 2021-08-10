d9b8061b9c68261692f8fecc7f36155df2e90551
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-08-10 12:37:50 +0100

    Add tests for following geps through function calls

---
+++ llvm/OverflowTool/tests/regression/follow_gep_two_equal.c
@@ -0,0 +1,25 @@
+// RUN: clang -g -c -O0 -Xclang -disable-O0-optnone -S -emit-llvm %s -o %t1.ll
+// RUN: opt -load-pass-plugin %libdir/libLLVMOverflowToolPass.so -aa-pipeline='basic-aa' -passes='oft-overflow-instrumentation' -S -o %t1.instrumented.ll %t1.ll 2> %t1.passout.ll
+// RUN: %bindir/check_marked_lines %t1.passout.ll 15 16
+
+#include <stdlib.h>
+#include <stdio.h>
+extern void oft_mark_(void *);
+
+void __attribute__ ((noinline)) markem(int *a) {
+    oft_mark_(&a[1]);
+    oft_mark_(&a[3]);
+}
+
+void __attribute__ ((noinline)) fn(int *x, int *y) {
+    x[1] += 1;
+    y[3] -= 1;
+}
+
+int main(int argc, char** argv) {
+    int *n = malloc(4 * sizeof(int));
+
+    markem(n);
+    fn(n, n);
+    return 0;
+}
