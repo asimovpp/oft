432f158ca8657690e76210b9c2e6f136f2cae810
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2021-10-20 18:46:03 +0100

    Fix and expand fortran allocation test cases

---
+++ llvm/OverflowTool/tests/regression/allocate_dummy_test.f90
@@ -0,0 +1,25 @@
+! RUN: flang -c -O2 -Xclang -disable-O0-optnone -S -emit-llvm %s -o %t1.ll
+! RUN: opt -load %libdir/libLLVMOverflowToolPass.so -oft-debug -load-pass-plugin %libdir/libLLVMOverflowToolPass.so -aa-pipeline='basic-aa' -passes='oft-overflow-instrumentation' -S -o %t1.instrumented.ll %t1.ll 2>%t1.passout.ll 
+! RUN: %bindir/check_marked_lines %t1.passout.ll 23 24
+
+
+!! this pattern can't be traced at the moment.
+!! You trace the dummy but miss it being save to the array. 
+program use_module
+    implicit none
+    external oft_mark
+    integer dummy1, dummy2, dummy3
+    integer, dimension (:), allocatable :: array
+    
+    allocate(array(4))
+    !call oft_mark(dummy1)
+    call oft_mark(dummy2)
+    call oft_mark(dummy3)
+    !array(1) = dummy1
+    array(2) = dummy2
+    array(3) = dummy3
+
+    !write(*,*) "Result 1: ", 3 * array(1) !mul should be marked
+    write(*,*) "Result 1: ", 5 * array(2) !mul should be marked
+    write(*,*) "Result 2: ", 7 * array(3) !mul should be marked
+end program use_module
--- llvm/OverflowTool/tests/regression/allocate_test.f90
+++ llvm/OverflowTool/tests/regression/allocate_test.f90
@@ -1,6 +1,6 @@
-! RUN: flang -g -c -O0 -Xclang -disable-O0-optnone -S -emit-llvm %s -o %t1.ll
-! RUN: opt -load-pass-plugin %libdir/libLLVMOverflowToolPass.so -aa-pipeline='basic-aa' -passes='oft-overflow-instrumentation' -S -o %t1.instrumented.ll %t1.ll 2>%t1.passout.ll
-! RUN: %bindir/check_marked_lines %t1.passout.ll 15 16 
+! RUN: flang -c -O0 -Xclang -disable-O0-optnone -S -emit-llvm %s -o %t1.ll
+! RUN: opt -load %libdir/libLLVMOverflowToolPass.so -oft-debug -load-pass-plugin %libdir/libLLVMOverflowToolPass.so -aa-pipeline='basic-aa' -passes='oft-overflow-instrumentation' -S -o %t1.instrumented.ll %t1.ll 2>%t1.passout.ll
+! RUN: %bindir/check_marked_lines %t1.passout.ll 17 18
 
 
 program use_module
@@ -8,10 +8,12 @@ program use_module
     external oft_mark
     integer, dimension (:), allocatable :: array
     
-    allocate(array(2))
-    call oft_mark(array(1))
+    allocate(array(4))
+    !call oft_mark(array(1))
     call oft_mark(array(2))
+    call oft_mark(array(3))
 
-    write(*,*) "Result 1: ", 3 * array(1) !mul should be marked
-    write(*,*) "Result 2: ", 7 * array(2) !mul should be marked
+    !write(*,*) "Result 1: ", 3 * array(1) !mul should be marked
+    write(*,*) "Result 1: ", 5 * array(2) !mul should be marked
+    write(*,*) "Result 2: ", 7 * array(3) !mul should be marked
 end program use_module
